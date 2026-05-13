52f1ce66813583e8275c3bbb1cb62a32674aaa4f
Namra <nm989781@dal.ca>
2026-05-13 16:25:22 -0300

    removed lab1 and lab2 from lab3 branch

--- lab1/logger-lab/src/task3f.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-# 1. Setup the format and level
-logging.basicConfig(format='%(levelname)s:%(name)s:%(message)s', level=logging.WARNING)
-
-# 2. Fire the message
-logging.warning('This is a warning')
